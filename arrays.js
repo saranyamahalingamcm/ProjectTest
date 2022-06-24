895ee50471fe40efc6ad1d63ba6d58e6aa551216
saranyamahalingamcm <98201995+saranyamahalingamcm@users.noreply.github.com>
2022-06-24 10:43:34 +0530

    test commit

---
+++ arrays.js
@@ -0,0 +1,63 @@
+//const numbers = [1, 2, 3, 1, 4, 3];
+//Add at end
+//numbers.push(5, 6);
+
+//Beginning 
+//numbers.unshift(1, 2);
+//Middle
+// numbers.splice(2, 0, 'a', 'b');
+// console.log(numbers);
+
+// console.log(numbers.indexOf(3));
+// console.log(numbers.lastIndexOf(3));
+// console.log(numbers.includes(5));
+
+//Exercise 1
+//  const numbers = arrayFromRange(0,4);
+// console.log(numbers);
+
+//  function arrayFromRange(min, max){
+//      const range = [];
+//      for (let i = min; i<=max; i++){
+//          range.push(i);         
+//      }
+//      return range;
+//  }
+
+
+//Find array
+// const namelists  = [
+//     { id : 1, name : 'a'},
+//     { id : 2, name : 'b'},
+// ]
+
+//Arrow fn-const findArray = findObject.find( findObject=> findObject.name==='b');
+//const name = namelists.find(function(id) { //id passed as parameter in the function can be of anyname
+  //      return id.id=== 2; //1st - id-each obj in the array, 2nd id - properties of the array obj
+ //   });
+// FindIndex - const findArray = findObject.findIndex( findObject=> findObject.name==='b');
+ //console.log(name);
+
+
+// const numbers= [ 1, 2, 3, 4];
+// let result = includes(numbers, 1);
+// console.log(result);
+
+// function includes(array, searchElement){
+//     for(let element of array){
+//         if (element === searchElement){
+//             return true;
+//         }      
+//     }
+//     return false;
+// }
+
+// const numbers= [ 1, 3, 2, 4];
+
+// numbers.sort();
+// console.log(numbers);
+
+// numbers.reverse();
+// console.log(numbers);
+
+ 
